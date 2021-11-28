af5af6d934129dbf6b5d38bc9a9f54c78ba218f6
roman-oleynik <raman.alejnik@gmail.com>
2021-11-28 20:08:22 +0300

    feat: add logic of requests handling

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const data = require("./data");
+const qs = require("qs");
 
 const app = (req, res) => {
     res.setHeader("Content-Type", "text/html; charset=utf-8;");
 
     if (req.url.match(/^\/person\/?$/) && req.method === "GET") {
-        
         res.write(JSON.stringify(data));
     } else if (req.url.match(/^\/person\/?$/) && req.method === "POST") {
         let body = "";
@@ -12,27 +12,49 @@ const app = (req, res) => {
             body += chunk;
         });
         req.on('end', () => {
-            data.push(JSON.parse(body));
-        })
+            const person = data.find(_ => _.id === qs.parse(`${body}`).id);
+            if (person) {
+                res.statusCode = 404;
+                res.write("Error: Person with this id already exists");
+            } else {
+                data.push(qs.parse(`${body}`));
+                res.write(JSON.stringify(qs.parse(`${body}`)));
+            }
+        });
     } else if (req.url.match(/^\/person\/[a-z0-9]+$/) && req.method === "GET") {
-        res.write(req.method);
-
-        res.write("Current person: " + req.url);
-    } else if (req.url.match(/^\/person\/[a-z0-9]+$/) && req.method === "POST") {
-        res.write(req.method);
-        res.write("Current person: " + req.url);
+            const person = data.find(_ => _.id === req.url.split("/").slice(-1)[0]);
+            if (person) {
+                res.write(JSON.stringify(person));
+            } else {
+                res.statusCode = 404;
+                res.write("Error: Person with this id doesn't exist");
+            }
     } else if (req.url.match(/^\/person\/[a-z0-9]+$/) && req.method === "PUT") {
-        res.write(req.method);
-        res.write("Current person: " + req.url);
+        const personIndex = data.findIndex(_ => _.id === req.url.split("/").slice(-1)[0]);
+        try {
+            let body = "";
+            req.on("data", chunk => {
+                body += chunk;
+            });
+            req.on('end', () => {
+                data[personIndex] = qs.parse(`${body}`);
+                res.write(JSON.stringify(qs.parse(`${body}`)));
+            })
+        } catch (err) {
+            res.statusCode = 404;
+            res.write("Error: " + err.message);
+        }
     } else if (req.url.match(/^\/person\/[a-z0-9]+$/) && req.method === "DELETE") {
-        res.write(req.method);
-        res.write("Current person: " + req.url);
+        const personIndex = data.find(_ => _.id === req.url.split("/").slice(-1)[0]);
+        data.splice(personIndex);
+        res.write("[]");
     } else {
         res.statusCode = 404;
         res.write("Error: Not Found");
     }
-
-    res.end();
+    setTimeout(() => {
+        res.end();
+    })
 };
 
 module.exports = app;
