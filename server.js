85785003d3d0b11926f4c2c40a3dc5e84df38d0d
roman-oleynik <raman.alejnik@gmail.com>
2021-11-28 22:00:25 +0300

    fix: adapt the functionality according to the task

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const http = require("http");
 
 const app = require('./src/app');
 
-http.createServer(app).listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
+const server = http.createServer(app);
+
+server.listen(PORT, () =>
+console.log(`App is running on http://localhost:${PORT}`)
 );
+
+server.on("error", err => {
+  console.log("Error: " + err.message);
+})
